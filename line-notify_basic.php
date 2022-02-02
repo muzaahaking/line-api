664ae3f412c6dd99ca46eb17fa7a67deaef2048e
Kant Charoenjit <42685739+muzaahaking@users.noreply.github.com>
2022-02-02 13:39:38 +0700

    Create line-notify_basic.php

---
+++ line-notify_basic.php
@@ -0,0 +1,27 @@
+<?php 
+//BASIC API - CONTROLLER v0.1a by !KANT. 2/2/2022
+
+$url      = 'https://notify-api.line.me/api/notify';
+$token    = 'xw8M94b4DtL34QZBiwFPL5J7pFezBae4fhp6xKZnZz0';
+$headers  = [
+                'Content-Type: application/x-www-form-urlencoded',
+                'Authorization: Bearer '.$token
+            ];
+$params     = 'สวัสดีจาก PHP..';
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $params);
+
+$response = curl_exec($ch);
+
+curl_close($ch);
+
+// var_dump($response);
+
+$result = json_decode($response,TRUE);
+
+print($result)
