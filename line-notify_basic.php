4c5b2370face59894534fd758cce49d396b703dc
Kant Charoenjit <42685739+muzaahaking@users.noreply.github.com>
2022-02-02 13:55:24 +0700

    Update line-notify_basic.php

--- line-notify_basic.php
+++ line-notify_basic.php
@@ -1,20 +1,20 @@
 <?php 
 //BASIC API - CONTROLLER v0.1a by !KANT. 2/2/2022
 
-$url      = 'https://notify-api.line.me/api/notify';
-$token    = 'xw8M94b4DtL34QZBiwFPL5J7pFezBae4fhp6xKZnZz0';
+$url      = "https://notify-api.line.me/api/notify";
+$token    = "xw8M94b4DtL34QZBiwFPL5J7pFezBae4fhp6xKZnZz0";
 $headers  = [
-                'Content-Type: application/x-www-form-urlencoded',
-                'Authorization: Bearer '.$token
+                "Content-Type: application/x-www-form-urlencoded",
+                "Authorization: Bearer ".$token
             ];
-$params     = 'สวัสดีจาก PHP..';
+$params     = "สวัสดีจาก PHP..";
 
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_POST, 1);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $params);
+curl_setopt($ch, CURLOPT_POSTFIELDS, "message=".$params);
 
 $response = curl_exec($ch);
 
